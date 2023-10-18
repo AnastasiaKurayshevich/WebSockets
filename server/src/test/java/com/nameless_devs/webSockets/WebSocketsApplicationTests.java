416c3ca97a8d4d4522ba3686c8bb575e55d9675e
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2023-10-18 13:48:56 +0200

    Initialized string project

---
+++ server/src/test/java/com/nameless_devs/webSockets/WebSocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nameless_devs.webSockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
